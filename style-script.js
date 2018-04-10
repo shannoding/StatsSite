52bbe7e26267ae7cb619ffdfb0129b969317a795
Shannon Ding <shannon_ding@yahoo.com>
2018-04-10 09:50:31 -0700

    many things including a nav dropdown but the width is strange

---
+++ style-script.js
@@ -0,0 +1,55 @@
+var layer1 = document.getElementById("Layer1");
+var layer2 = document.getElementById("Layer2");
+var layer3 = document.getElementById("Layer3");
+var navButton = document.getElementById("nav-drop-button");
+
+var originalNavHTML = document.getElementById("Layer1").innerHTML;
+var homeNavHTML = '<li class="navBox">Home</li>';
+var algebraNavHTML = '<li class="navBox"><span class="navHeader">Algebra IB</span><ul><li>Grades<ul><li>Check Grades</li></ul></li><li>Assignments</li><li>Resources</li><li>Course Information</li></ul></li>';
+var statsNavHTML = '<li class="navBox"><span class="navHeader">AP Statistics</span><ul><li>Grades<ul><li>Check Grades</li></ul></li><li>Assignments</li><li>Resources</li><li>Course Information</li></ul></li>';
+var cahseeNavHTML = '<li class="navBox">CAHSEE Guide</li>';
+var parentResourcesNavHTML = '<li class="navBox">Parent Resources</li>';
+var emailNavHTML = '<li class="navBox">E-mail Mr. Iams</li>';
+var navHTML = '<div id="nav"><ul>' + homeNavHTML + algebraNavHTML + statsNavHTML + cahseeNavHTML + parentResourcesNavHTML + emailNavHTML + '</ul></div>';
+console.log(navHTML);
+var navButtonHTML = '<div id="nav-drop-button"><span></span><span></span><span></span></div>';
+var mobile = false;
+
+init();
+
+function init() {
+  window.addEventListener("resize", updateNav);
+  if (window.innerWidth < 768) {
+    updateNav();
+  }
+}
+function updateNav() {
+  if (window.innerWidth < 768) {
+    if (!mobile) {
+      console.log("switched to mobile");
+      layer1.innerHTML = navHTML;
+      layer3.innerHTML += navButtonHTML;
+      navButton = document.getElementById("nav-drop-button");
+      navButton.addEventListener("click", dropdown);
+    }
+    mobile = true;
+  }
+  else {
+    if (mobile) {
+      console.log("switched to desktop");
+      navButton.removeEventListener("click", dropdown);
+      dropup();
+      layer1.innerHTML = originalNavHTML;
+      layer3.removeChild(layer3.lastChild);
+    }
+    mobile = false;
+  }
+}
+
+
+function dropdown() {
+  layer1.classList.toggle("dropdown");
+}
+function dropup() {
+  layer1.classList.remove("dropdown");
+}
