f0c2c85bf53f317a708130b4f0f7865d63f75e0d
Shannon Ding <shannon_ding@yahoo.com>
2018-04-10 17:18:51 -0700

    fixed the inner dropdown and added allinks so it's p much done

--- style-script.js
+++ style-script.js
@@ -5,12 +5,12 @@ var navButton = document.getElementById("nav-drop-button");
 var mobileWidth = 768;
 
 var originalNavHTML = document.getElementById("Layer1").innerHTML;
-var homeNavHTML = '<li class="navBox">Home</li>';
-var algebraNavHTML = '<li class="navBox"><span class="navHeader">Algebra IB</span><ul class="hiddenNav"><li>Grades<ul><li>Check Grades</li></ul></li><li>Assignments</li><li>Resources</li><li>Course Information</li></ul></li>';
-var statsNavHTML = '<li class="navBox"><span class="navHeader">AP Statistics</span><ul class="hiddenNav"><li>Grades<ul><li>Check Grades</li></ul></li><li>Assignments</li><li>Resources</li><li>Course Information</li></ul></li>';
-var cahseeNavHTML = '<li class="navBox">CAHSEE Guide</li>';
-var parentResourcesNavHTML = '<li class="navBox">Parent Resources</li>';
-var emailNavHTML = '<li class="navBox">E-mail Mr. Iams</li>';
+var homeNavHTML = '<li class="navBox"><a href="index.html">Home</a></li>';
+var algebraNavHTML = '<li class="navBox"><div class="navHeader moreDrop">Algebra IB</div><ul class="hideNav hiddenNav"><li><a href="grades.html">Grades</a></li><li><a href="http://www.lhs.kennyiams.com/InternetGrades/index.html">Check Grades</a></li><li><a href="alg_assign.html">Assignments</a></li><li><a href="alg_resources.htm">Resources</a></li><li><a href="FilesAlg/01D_Green%20Sheet%20AlgIB.pdf">Course Information</a></li></ul></li>';
+var statsNavHTML = '<li class="navBox"><div class="navHeader moreDrop">AP Statistics</div><ul class="hideNav hiddenNav"><li><a href="grades.html">Grades</a></li><li><a href="http://www.lhs.kennyiams.com/InternetGrades/index.html">Check Grades</a></li><li><a href="APStats_assign.htm">Assignments</a></li><li><a href="APStats_resources.htm">Resources</a></li><li><a href="FilesAPStats/01D_Green%20Sheet%20APStats.pdf">Course Information</a></li></ul></li>';
+var cahseeNavHTML = '<li class="navBox"><a href="CAHSEE/flashcards_CAHSEE.htm">CAHSEE Guide</a></li>';
+var parentResourcesNavHTML = '<li class="navBox"><a href="parent.html">Parent Resources</a></li>';
+var emailNavHTML = '<li class="navBox"><a href="mailto:ken_iams@fuhsd.org">E-mail Mr. Iams</a></li>';
 var navHTML = '<div id="nav"><ul>' + homeNavHTML + algebraNavHTML + statsNavHTML + cahseeNavHTML + parentResourcesNavHTML + emailNavHTML + '</ul></div>';
 console.log(navHTML);
 var navButtonHTML = '<div id="nav-drop-button"><span></span><span></span><span></span></div>';
@@ -23,6 +23,12 @@ function init() {
   window.addEventListener("resize", updateNav);
   if (window.innerWidth < mobileWidth) {
     updateNav();
+    var moreDrops = layer1.getElementsByClassName("moreDrop");
+    for (var i = 0; i < moreDrops.length; i++) {
+      moreDrops[i].addEventListener("click", function() {
+        dropMoreDown(this);
+      });
+    }
   }
 }
 function updateNav() {
@@ -54,4 +60,11 @@ function dropdown() {
 }
 function dropup() {
   layer1.classList.remove("dropdown");
+}
+
+function dropMoreDown(item) {
+  var toDrop = item.parentElement.getElementsByClassName("hideNav");
+  for (var i = 0; i < toDrop.length; i++) {
+    toDrop[i].classList.toggle("hiddenNav");
+  }
 }
